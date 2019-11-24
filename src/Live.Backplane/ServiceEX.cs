6e8b69b6747117468df05fa8de7fa7704c89e60d
Hassan Hashemi <hassanhashemi@yahoo.com>
2019-11-24 19:22:41 +0330

    Dev'

---
+++ src/Live.Backplane/ServiceEX.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+using System;
+
+namespace Live.Backplane
+{
+    public static class ServiceEx
+    {
+        public static void AddRedisBackplane(this IServiceCollection services, string config)
+        {
+            if (string.IsNullOrEmpty(config))
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            services.AddSingleton<IBackplaine, RedisBackplaine>();
+            services.AddSingleton<IConnectionMultiplexer>(
+                o => ConnectionMultiplexer.Connect(config));
+        }
+    }
+}
